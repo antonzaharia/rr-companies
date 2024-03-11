9c9eaa09240b6e51b2d022a4259ceebf49adfab3
antonzaharia <zaharia_anton@yahoo.com>
2024-03-11 10:14:27 +0000

    feat: companies controller filters

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::CompaniesController < ApplicationController
   def index
-    companies = Company.all.order(created_at: :desc)
-    render json: companies.as_json(include: :deals)
+    companies = Company.filtered_companies(permitted_params)
+
+    render json: {
+      companies: ActiveModelSerializers::SerializableResource.new(companies, each_serializer: CompanySerializer).as_json
+    }
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:companyName, :industry, :minEmployee, :minimumDealAmount)
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,10 @@ class Company < ApplicationRecord
   include Filterer
 
   has_many :deals
+
+  def self.filtered_companies(params)
+    includes(:deals)
+      .filtered(params.except(:page))
+      .order(created_at: :desc)
+  end
 end
