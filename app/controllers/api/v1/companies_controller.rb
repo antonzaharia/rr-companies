e9f28f4848a39518adbff08a38bcc36219b878c8
antonzaharia <zaharia_anton@yahoo.com>
2024-03-11 10:22:12 +0000

    feat: paginate companies results

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -3,13 +3,17 @@ class Api::V1::CompaniesController < ApplicationController
     companies = Company.filtered_companies(permitted_params)
 
     render json: {
-      companies: ActiveModelSerializers::SerializableResource.new(companies, each_serializer: CompanySerializer).as_json
+      companies: ActiveModelSerializers::SerializableResource.new(companies, each_serializer: CompanySerializer).as_json,
+      meta: {
+        page: companies.current_page,
+        pages: companies.total_pages
+      }
     }
   end
 
   private
 
   def permitted_params
-    params.permit(:companyName, :industry, :minEmployee, :minimumDealAmount)
+    params.permit(:companyName, :industry, :minEmployee, :minimumDealAmount, :page)
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,15 @@
 class Company < ApplicationRecord
+  self.per_page = 10
   include Filterer
 
   has_many :deals
 
+  scope :order_by_creation_desc, -> { order(created_at: :desc) }
+
   def self.filtered_companies(params)
     includes(:deals)
       .filtered(params.except(:page))
-      .order(created_at: :desc)
+      .order_by_creation_desc
+      .paginate(page: params[:page])
   end
 end
