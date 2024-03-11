ef96dc202078c666b962582dff34ee38008cdda7
antonzaharia <zaharia_anton@yahoo.com>
2024-03-11 09:57:09 +0000

    feat: company serializer implementation

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,7 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :industry, :employee_count, :total_deals_amount
+
+  def total_deals_amount
+    object.deals.sum(&:amount)
+  end
 end
