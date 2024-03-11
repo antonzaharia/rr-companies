4afccceeea40972d5d2ff4a43b086f9acbaea9b6
antonzaharia <zaharia_anton@yahoo.com>
2024-03-11 17:47:38 +0000

    feat: filter by min deal total amount

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,11 +5,25 @@ class Company < ApplicationRecord
   has_many :deals
 
   scope :order_by_creation_desc, -> { order(created_at: :desc) }
+  scope :with_total_deals_amount, -> {
+    joins(:deals)
+      .group(:id)
+      .select("companies.*, SUM(deals.amount) AS total_deals_amount")
+  }
+  scope :with_minimum_deal_amount, ->(min_amount) {
+    joins(:deals)
+      .group('companies.id')
+      .having('SUM(deals.amount) >= ?', min_amount)
+  }
 
-  def self.filtered_companies(params)
-    includes(:deals)
+  def self.filtered_and_paginated(params)
+    with_total_deals_amount
       .filtered(params.except(:page))
       .order_by_creation_desc
       .paginate(page: params[:page])
+  end
+
+  def total_deals_amount
+    self.read_attribute(:total_deals_amount) || deals.sum(:amount)
   end
 end
--- app/models/concerns/filterer.rb
+++ app/models/concerns/filterer.rb
@@ -10,7 +10,7 @@ module Filterer
         method(:filter_by_name),
         method(:filter_by_industry),
         method(:filter_by_min_employee_count),
-        method(:filter_by_minimum_deal_amount)
+        method(:filter_by_minimum_deal_total_amount)
       ]
 
       filters_chain.each do |filter|
@@ -26,29 +26,28 @@ module Filterer
     def filter_by_name(results, params)
       return results unless params[:companyName].present?
 
-      results.where("name LIKE ?", "%#{params[:companyName]}%")
+      results.where("companies.name LIKE ?", "%#{params[:companyName]}%")
     end
 
     # Filtering by industry
     def filter_by_industry(results, params)
       return results unless params[:industry].present?
 
-      results.where("industry LIKE ?", "%#{params[:industry]}%")
+      results.where("companies.industry LIKE ?", "%#{params[:industry]}%")
     end
 
     # Filtering by min employee count
     def filter_by_min_employee_count(results, params)
       return results unless params[:minEmployee].present?
 
-      results.where("employee_count >= ?", params[:minEmployee])
+      results.where("companies.employee_count >= ?", params[:minEmployee])
     end
 
-    # Filtering by min deal amount
-    def filter_by_minimum_deal_amount(results, params)
+    # Filtering by min deal total amount
+    def filter_by_minimum_deal_total_amount(results, params)
       return results unless params[:minimumDealAmount].present?
 
-      ids = Deal.where('amount >= ?', params[:minimumDealAmount]).distinct.pluck(:company_id)
-      results.where(id: ids)
+      results.with_minimum_deal_amount(params[:minimumDealAmount])
     end
   end
 end
